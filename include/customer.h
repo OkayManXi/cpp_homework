9fd972b59d7da9ebdae72979bde85df8a689e589
OkayManXi <786783021@qq.com>
2020-11-25 15:31:46 +0800

    c++ group work

---
+++ include/customer.h
@@ -0,0 +1,16 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <iostream>
+
+#include "cmachine.h"
+
+using namespace std;
+
+class Customer
+{
+  public:
+    void buyDrink(CMachine &v);
+};
+
+#endif
